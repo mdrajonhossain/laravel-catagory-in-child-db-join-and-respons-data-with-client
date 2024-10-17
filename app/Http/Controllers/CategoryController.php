8b59e505a31f5882ea9ab908db76fbf088eaabaf
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-17 14:01:14 +0600

    teaa

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,6 +10,7 @@ class CategoryController extends Controller{
 
     public function index(){
         $categories = Category::whereNull('parent_id')->with('children')->get();
+        // $categories = Category::whereNull('parent_id')->with('children')->where('id', 1)->get();
         return response()->json($categories);
     }
 }
